1c405a1c92e5c7d6c926693fbc3e873806381b6b
Mauricio Gomez <mauricio.gomez.15@hotmail.com>
2026-09-24 16:17:51 -0300

    Put height mobile

--- src/components/Pages/PageStyles.ts
+++ src/components/Pages/PageStyles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContainerPage = styled.main`
   width: 100%;
-  height: calc(100vh - 70px);
+  height: calc(100dvh - 70px);
   margin: 70px 0 0;
   padding: 25px;
 
